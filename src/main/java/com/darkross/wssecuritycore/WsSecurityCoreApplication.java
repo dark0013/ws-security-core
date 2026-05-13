e7bc2071c32441cb62ee6df4fc46386b18ba73ba
dark0013 <andresarcangel00@gmail.com>
2026-05-13 00:34:33 -0500

    generacion del proyecto

---
+++ src/main/java/com/darkross/wssecuritycore/WsSecurityCoreApplication.java
@@ -0,0 +1,13 @@
+package com.darkross.wssecuritycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSecurityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsSecurityCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkross/wssecuritycore/WsSecurityCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkross.wssecuritycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSecurityCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
